e9b3451354b7e1ac14c19ab6a28c103d038b4a4e
yier <yier@aisingapore.org>
2024-05-03 15:26:26 +0800

    add dataset download code to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,8 @@
 
 set -euo pipefail
 
+[ -d datasets/m_arc ] || git clone "https://${HF_USER}:${HF_TOKEN}@huggingface.co/datasets/aisingapore/m_arc" datasets/m_arc
+[ -d datasets/m_hellaswag ] || git clone "https://${HF_USER}:${HF_TOKEN}@huggingface.co/datasets/aisingapore/m_hellaswag" datasets/m_hellaswag
+[ -d datasets/m_mmlu ] || git clone "https://${HF_USER}:${HF_TOKEN}@huggingface.co/datasets/aisingapore/m_mmlu" datasets/m_mmlu
+
 python main.py "$@"
